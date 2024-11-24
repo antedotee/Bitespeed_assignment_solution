ac89e318d9ffa1d263712d82fd006bbad99bca59
antedotee <soniyadav2051982@gmail.com>
2024-11-24 10:32:53 +0530

    fixed the endpoint

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const contactRoutes = require("./src/routes/contactRoutes");
 const app = express();
 
 app.use(express.json());
-app.use("/api/contacts", contactRoutes);
+app.use("", contactRoutes);
 
 module.exports = app;
